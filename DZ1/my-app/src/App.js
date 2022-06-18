5bcae602862aebbbe988bef017440384b9322e9f
Bekbolsun <linex1827@gmail.com>
2022-06-18 14:05:18 +0600

    DZ1

---
+++ DZ1/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AboutUs from "./pages/aboutUs/AboutUs";
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <AboutUs text="lorem "/>
+    </div>
+  );
+}
+
+export default App;
