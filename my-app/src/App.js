ac6697f24e259325233f3f889c530dabdc0f67b1
Bekbolsun <linex1827@gmail.com>
2022-07-02 17:51:53 +0600

    dz6

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import ProductPage from "./pages/productPage/ProductPage";
-
-
+import MainPage from "./pages/MainPages/MainPages";
 
 function App() {
   return (
     <div className="App">
-        <ProductPage/>
+      <MainPage/>
     </div>
   );
 }
